8b939e5a873b0ec20b4359deea45338e20b5a415
Thomas CASTELLY <shenron@tcy.io>
2017-01-10 15:40:35 +0100

    Add compatibility with PHP 5.6

--- nginx_php_fpm_oracle_client/conf/entrypoint.sh
+++ nginx_php_fpm_oracle_client/conf/entrypoint.sh
@@ -2,9 +2,9 @@
 
 # For DEV env, disabled opc cache
 if [ "$ENV" == "DEV" ]; then
-    sed -i "s/opcache.revalidate_freq=60/opcache.revalidate_freq=0/" /etc/php/7.1/fpm/php.ini
+    sed -i "s/opcache.revalidate_freq=60/opcache.revalidate_freq=0/" /etc/php5/fpm/php.ini
 fi
 
-/etc/init.d/php7.1-fpm start
+/etc/init.d/php5-fpm start
 
 exec "$@"
