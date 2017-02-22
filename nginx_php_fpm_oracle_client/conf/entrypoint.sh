5fd5fd6607f6fe16880916caaa943ba47d09354c
Thomas CASTELLY <shenron@tcy.io>
2017-02-22 16:09:48 +0100

    extends entrypoint with entrypoint-nginx-conf

--- nginx_php_fpm_oracle_client/conf/entrypoint.sh
+++ nginx_php_fpm_oracle_client/conf/entrypoint.sh
@@ -24,6 +24,8 @@ if [ "$ENV" == "DEV" ]; then
   sed -i "s/opcache.revalidate_freq=60/opcache.revalidate_freq=0/" /etc/php5/fpm/php.ini
 fi
 
+/entrypoint-nginx-conf.sh
+
 /etc/init.d/php5-fpm start
 
 exec "$@"
