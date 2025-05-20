45174fe0bb853ab13b749e01571812ad261883d5
Thomas CASTELLY <thomas@castelly.fr>
2025-05-20 09:25:59 +0200

    fix xdebug config

--- nginx_php_fpm_oracle_client/conf/entrypoint.sh
+++ nginx_php_fpm_oracle_client/conf/entrypoint.sh
@@ -12,16 +12,16 @@ if [ "$PHP_ENV" == "development" ]; then
   # On linux it's possible to try to connect to the client
   # On MacOS X, the IP of host have to be set
   if [ -z "$IP_HOST" ]; then
-    echo xdebug.remote_connect_back=1 >> $xdebugPath/xdebug.ini
+    echo xdebug.discover_client_host=1 >> $xdebugPath/xdebug.ini
   else
-    echo xdebug.remote_connect_back=0 >> $xdebugPath/xdebug.ini
+    echo xdebug.discover_client_host=0 >> $xdebugPath/xdebug.ini
     echo "xdebug.remote_host=$IP_HOST" >> $xdebugPath/xdebug.ini
   fi
 
-  echo xdebug.remote_enable=1 >> $xdebugPath/xdebug.ini
-  echo xdebug.remote_autostart=0 >> $xdebugPath/xdebug.ini
-  echo xdebug.remote_port=9000 >> $xdebugPath/xdebug.ini
-  echo xdebug.remote_log=/tmp/php-xdebug.log >> $xdebugPath/xdebug.ini
+  echo xdebug.mode=debug >> $xdebugPath/xdebug.ini
+  echo xdebug.start_with_request=0 >> $xdebugPath/xdebug.ini
+  echo xdebug.client_port=9000 >> $xdebugPath/xdebug.ini
+  echo xdebug.log=/tmp/php-xdebug.log >> $xdebugPath/xdebug.ini
 
   sed -i "s/opcache.revalidate_freq=60/opcache.revalidate_freq=0/" /etc/php/$PHP_V_PATH/fpm/php.ini
 fi
